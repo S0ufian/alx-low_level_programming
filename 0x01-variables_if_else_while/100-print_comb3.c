01bf7503088f3e44f2209f364e391b58515c2727
Soufian <benchakrounes18@gmail.com>
2023-12-15 14:26:12 -0800

    Tasks 10 to 13

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 48; digit1 <= 56; digit1++)
+	{
+		for (digit2 = 49; digit2 <= 57; digit2++)
+		{
+			if (mdigit2 > ndigit1)
+			{
+				putchar(digit1);
+				putchar(digit2);
+				if (digit1 != 56 || digit2 != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit1, digit2, digit3;
+
+	for (digit1 = 48; digit1 < 58; digit1++)
+	{
+		for (digit2 = 49; digit2 < 58; digit2++)
+		{
+			for (digit3 = 50; digit3 < 58; digit3++)
+			{
+				if (digit3 > mdigit2 && mdigit2 > digit1)
+				{
+					putchar(ndigit1);
+					putchar(mdigit2);
+					putchar(digit3);
+					if (ndigit1 != 55 || mdigit2 != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
